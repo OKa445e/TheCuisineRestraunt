ace8f04c61f438865b13c0b44a440a2007fe5131
Atharv sinha <87602922+OKa445e@users.noreply.github.com>
2025-02-06 22:56:17 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,20 +1,35 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react"; 
 import { CDN_URL } from "../utils/contants";
 import { addItem } from "../utils/cartSlice";
 
 const ItemList = ({ items }) => {
   const dispatch = useDispatch();
+
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleAddItem = (item) => {
     dispatch(addItem(item));
+
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
   };
 
   return (
     <div>
+      {showAlert && (
+        <div className="fixed top-0 left-0 w-full bg-green-500 text-white text-center py-3 text-sm font-semibold z-50 opacity-100 transition-opacity duration-1000 ease-in-out">
+          Item added to cart!
+        </div>
+      )}
+
       {items?.map((item) => {
         return (
           <div
             key={item.card.info.id}
-            className="p-2 m-2 border-gray-200 border-b-2 text-left flex items-center justify-between"
+            className="p-2 m-2 border-b-2 border-gray-200 text-left flex items-center justify-between"
           >
             <div className="w-9/12">
               <span className="font-semibold">{item.card.info.name}</span>
@@ -30,7 +45,6 @@ const ItemList = ({ items }) => {
             </div>
 
             <div className="w-3/12 relative">
-              {/* Button positioned on the right */}
               <button
                 className="absolute right-0 p-1 px-3 rounded-lg bg-black text-white shadow-md text-xs cursor-pointer"
                 onClick={() => handleAddItem(item)}
